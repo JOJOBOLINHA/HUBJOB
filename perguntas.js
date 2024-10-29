96546f579440f9f721d4f9452d94c2629294019f
JOJOBOLINHA <140609863+JOJOBOLINHA@users.noreply.github.com>
2024-10-29 08:49:30 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,64 @@
+criaCartao(
+    `Química` ,
+    `Alcinos são hidrocarbonetos...` ,
+    `alifáticos insaturados com tripla ligação.`
+)
+criaCartao(
+    `Matemática` ,
+    `Qual é o cosseno de 90?` ,
+    `0`
+)
+criaCartao(
+    `Biologia` , 
+    `Quantos pares de cromossomos temos em uma célula?` ,
+    `23 pares`
+)
+criaCartao(
+    `L.P` ,
+    `Paralelepípedo é uma ?` ,
+    `Proparoxítona`
+)
+criaCartao(
+    `L.I` ,
+    `Quando usar o "on" em inglês?` ,
+    `deve ser usada para anteceder locais (superfícies); dias específicos; referência; condição e tempo.`
+)
+criaCartao(
+    `Programação` ,
+    `O que é header ?` ,
+    `cabeçalho`
+)
+criaCartao(
+    `Educação Financeira` ,
+    `Qual a moeda utilizada no Japão ?` ,
+    `Iene`
+)
+criaCartao(
+    `Geografia` ,
+    `O que visava o Taylorismo ?` ,
+    `Diminuir o tempo de produção`
+)
+criaCartao(
+    `História` ,
+    `Quem foi Benito Mussolini?` ,
+    `Líder o facismo italiao`
+)
+ criaCartao(
+    `Empreendedorismo` ,
+    `Cite um beneício do CLT` ,
+    `13º salário, férias remuneradas, FGTS, contribuição ao INSS, vale-transporte e licença maternidade.`      
+ )
+ criaCartao(
+    `Física` ,
+    `O que é um potenciometro ?` ,
+    `Os potenciômetros são resistores de elevada precisão com uma derivação que permite a variação do valor resistivo pelo movimento de um eixo.`
+ )
+criaCartão(
+    `Sociologia` ,
+)
+                    <h3>Sociologia</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p> Qual era as ações de Weber? </p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>ação social racional com relação a fins, ação social racional com relação a valores, ação social afetiva e ação social tradicional.</p>
