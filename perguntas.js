4ebf8ccc0446ddbc301157a6262218c954d190d8
JOJOBOLINHA <140609863+JOJOBOLINHA@users.noreply.github.com>
2024-11-05 08:42:35 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -4,57 +4,57 @@ criaCartao(
     'alifáticos insaturados com tripla ligação.'
 )
 criaCartao(
-    `Matemática` ,
-    `Qual é o cosseno de 90?` ,
-    `0`
+    'Matemática' ,
+    'Qual é o cosseno de 90?' ,
+    '0'
 )
 criaCartao(
-    `Biologia` , 
-    `Quantos pares de cromossomos temos em uma célula?` ,
-    `23 pares`
+    'Biologia' , 
+    'Quantos pares de cromossomos temos em uma célula?' ,
+    '23 pares'
 )
 criaCartao(
-    `L.P` ,
-    `Paralelepípedo é uma ?` ,
-    `Proparoxítona`
+    'L.P' ,
+    'Paralelepípedo é uma ?' ,
+    'Proparoxítona'
 )
 criaCartao(
-    `L.I` ,
-    `Quando usar o "on" em inglês?` ,
-    `deve ser usada para anteceder locais (superfícies); dias específicos; referência; condição e tempo.`
+    'L.I' ,
+    'Quando usar o "on" em inglês?' ,
+    'deve ser usada para anteceder locais (superfícies); dias específicos; referência; condição e tempo.'
 )
 criaCartao(
-    `Programação` ,
-    `O que é header ?` ,
-    `cabeçalho`
+    'Programação' ,
+    'O que é header ?' ,
+    'cabeçalho'
 )
 criaCartao(
-    `Educação Financeira` ,
-    `Qual a moeda utilizada no Japão ?` ,
-    `Iene`
+    'Educação Financeira' ,
+    'Qual a moeda utilizada no Japão ?' ,
+    'Iene'
 )
 criaCartao(
-    `Geografia` ,
-    `O que visava o Taylorismo ?` ,
-    `Diminuir o tempo de produção`
+    'Geografia' ,
+    'O que visava o Taylorismo ?' ,
+    'Diminuir o tempo de produção'
 )
 criaCartao(
-    `História` ,
-    `Quem foi Benito Mussolini?` ,
-    `Líder o facismo italiao`
+    'História' ,
+    'Quem foi Benito Mussolini?' ,
+    'Líder o facismo italiano '
 )
  criaCartao(
-    `Empreendedorismo` ,
-    `Cite um beneício do CLT` ,
-    `13º salário, férias remuneradas, FGTS, contribuição ao INSS, vale-transporte e licença maternidade.`      
+    'Empreendedorismo' ,
+    'Cite um beneício do CLT' ,
+    '13º salário, férias remuneradas, FGTS, contribuição ao INSS, vale-transporte e licença maternidade.'     
  )
  criaCartao(
-    `Física` ,
-    `O que é um potenciometro ?` ,
-    `Os potenciômetros são resistores de elevada precisão com uma derivação que permite a variação do valor resistivo pelo movimento de um eixo.`
+    'Física' ,
+    'O que é um potenciometro ?' ,
+    'Os potenciômetros são resistores de elevada precisão com uma derivação que permite a variação do valor resistivo pelo movimento de um eixo.'
  )
 criaCartão(
-    `Sociologia` ,
-    `Qual era as ações de Weber?` ,
-    `ação social racional com relação a fins, ação social racional com relação a valores, ação social afetiva e ação social tradicional.`
+    'Sociologia' ,
+    'Qual era as ações de Weber?' ,
+    'ação social racional com relação a fins, ação social racional com relação a valores, ação social afetiva e ação social tradicional.'
 )
